1d0467c1daeec96c8a0ac9f45a0e2ee52cf31b02
Harikrishnanvkph <harikrishnanvk0531@gmail.com>
2023-12-28 13:57:28 +0530

    task_DOM_Calculator

--- calcFunction.js
+++ calcFunction.js
@@ -33,24 +33,35 @@ function foo(val){
                 Clear();
                 return;
             case "equal":
+                if(temp2===""){
+                    return;
+                }
                 input = triggerOperation(input, temp2, opt);
                 textArea.innerHTML =  input;
+                if(checkFactor(input)){
+                    dotOn = false;
+                }else{
+                    dotOn = true;
+                }
                 temp2 = "";
                 opt = "";
                 opOn = false;
-                current=="input"
+                current="input"
                 return;
             case "dot":
                 if(dotOn){
                     return;
                 }
-                if(current=="input"){
+                if(current==="input"){
                     input += ".";
                 }else{
                     temp2 += ".";
                 }
                 dotOn = true;
                 break;
+            case "backspace":
+                textArea.innerHTML =  splitString(textArea.innerHTML);
+                return;
         }
     }
     console.log(input);
@@ -59,19 +70,8 @@ function foo(val){
 }
 
 function textInputNumber(value){
-    switch(value){
-        case "one": return "1";
-        case "two": return "2";
-        case "three": return "3";
-        case "four": return "4";
-        case "five": return "5";
-        case "six": return "6";
-        case "seven": return "7";
-        case "eight": return "8";
-        case "nine": return "9";
-        case "zero": return "0";
-        case "dZero": return "00";
-    }
+    var res = document.getElementById(value);
+    return res.innerText;
 }
 
 function textInputOperator(value){
@@ -105,9 +105,58 @@ function triggerOperation(input, temp2, opt){
             result = (num1+num2).toFixed(2);
             break;
     }
-    if(((result * 10)%10) === 0){
+    if(checkFactor(result)){
         return parseInt(result).toFixed(0);
     }else{
         return result;
     }
+}
+
+function checkFactor(val){
+    if((val*10)%10 === 0){
+        return true;
+    }
+    return false;
+}
+
+function splitString(val){
+    let temp1 = "";
+    let temp2 = "";
+    let opt = "";
+    let optT = false;
+    let tempT = false;
+    for(let i=0;i<val.length;i++){
+        let ch = val.charAt(i);
+        if(ch){
+            dotOn = false;
+        }
+        if(ch === '+' || ch === '-' || ch === '/' || ch === 'X'){
+            if(i===0){
+                continue;
+            }
+            opt = ch + "";
+            optT = true;
+            opOn = false;
+        }else if(optT === true){
+            temp2 += ch;
+            tempT = true;
+        }else{
+            temp1 += ch;
+        }
+    }
+    if(!optT){
+        temp1 = temp1.slice(0,temp1.length-1)
+        input = temp1;
+        current = "input";
+    }else if(tempT){
+        input=temp1;
+        opt = opt;
+        temp2 = temp2.slice(0,temp2.length-1);
+        current = "temp";
+    } else if(!tempT && optT){
+        input=temp1;
+        opt = "";
+    }
+    let sum = temp1+opt+temp2;
+    return sum;
 }
